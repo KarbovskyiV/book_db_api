a64a8cb6168875beff6148b3f25f05e60df0e216
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2024-03-30 12:47:20 +0200

    Rename method

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/importCSV', [BookController::class, 'importCSV']);
+Route::post('/importCSVDataToDatabase', [BookController::class, 'importCSVDataToDatabase']);
